f18e0c5a025d587a355cb2a4c2feea3236f4aae5
Dipanjan De <dipanjan131@gmail.com>
2022-10-06 01:18:43 +0530

    Added Scripts

---
+++ figma.config.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const svgo = require("@figma-export/transform-svg-with-svgo");
+
+const fileId = process.env.FILE_ID;
+const outputters = [
+	require("@figma-export/output-components-as-svg")({ output: "./" }),
+];
+
+/** @type {import('svgo').PluginConfig[]} */
+// const solidSVGOConfig = [
+// 	{ removeDimensions: true },
+// 	{ sortAttrs: true },
+// 	{ removeAttrs: { attrs: "fill" } },
+// 	{ addAttributesToSVGElement: { attribute: { fill: "currentColor" } } },
+// ];
+const solidSVGOConfig = [];
+
+/** @type {import('svgo').PluginConfig[]} */
+// const outlineSVGOConfig = [
+// 	{ removeDimensions: true },
+// 	{ sortAttrs: true },
+// 	{ removeAttrs: { attrs: "stroke" } },
+// 	{ addAttributesToSVGElement: { attribute: { stroke: "currentColor" } } },
+// ];
+const outlineSVGOConfig = [];
+
+/** @type {import('@figma-export/types').FigmaExportRC} */
+module.exports = {
+	commands: [
+		[
+			"components",
+			{
+				fileId,
+				onlyFromPages: ["solid"],
+				transformers: [
+					svgo({ multipass: true, plugins: solidSVGOConfig }),
+				],
+				outputters,
+			},
+		],
+		[
+			"components",
+			{
+				fileId,
+				onlyFromPages: ["outline"],
+				transformers: [
+					svgo({ multipass: true, plugins: outlineSVGOConfig }),
+				],
+				outputters,
+			},
+		],
+	],
+};
